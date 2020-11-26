5758cf5051f787469c6db93decf0f5d9aecb2740
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-26 13:50:51 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -9,14 +9,7 @@ class Config(object):
     API_HASH = get_config("API_HASH")
      # Get these values from my.telegram.org
     # array to store the channel ID who are authorized to use the bot
-    AUTH_USERS = set(
-        int(x) for x in get_config(
-            "AUTH_USERS",
-            should_prompt=True
-        ).split()
-    )
     # the download location, where the HTTP Server runs
-    DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/DOWNLOADS")
     # Telegram maximum file upload size
     MAX_FILE_SIZE = 1572864000
     TG_MAX_FILE_SIZE = 1572864000
