735815fa99dd8528c24cda4d04864fe39548aa2c
Aditya <58950863+xditya@users.noreply.github.com>
2020-11-26 13:53:00 +0530

    Update incoming_message_fn.py

--- bot/plugins/incoming_message_fn.py
+++ bot/plugins/incoming_message_fn.py
@@ -14,8 +14,7 @@ LOGGER = logging.getLogger(__name__)
 import os, time, asyncio, json
 from bot.localisation import Localisation
 from bot import (
-  DOWNLOAD_LOCATION, 
-  AUTH_USERS
+  DOWNLOAD_LOCATION
 )
 from bot.helper_funcs.ffmpeg import (
   convert_video,
@@ -37,27 +36,9 @@ from bot.helper_funcs.utils import(
   delete_downloads
 )
         
-async def incoming_start_message_f(bot, update):
-    """/start command"""
-    if update.from_user.id not in AUTH_USERS:
-        await update.message.delete()
-        return
-    # LOGGER.info(update)
-    await bot.send_message(
-        chat_id=update.chat.id,
-        text=Localisation.START_TEXT,
-        reply_to_message_id=update.message_id
-    )
-    
 async def incoming_compress_message_f(bot, update):
   """/compress command"""
     
-  if update.from_user.id not in AUTH_USERS:
-    try:
-      await update.message.delete()
-    except:
-      pass
-    return
   if update.reply_to_message is None:
     try:
       await bot.send_message(
